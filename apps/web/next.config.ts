0716638dd745ea4642a242dbd092ca7d8cb48b15
Vansh Aggarwal <vanshaggarwal2004@gmail.com>
2025-09-14 22:53:37 +0530

    Added vercel config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,17 +1,23 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import type { NextConfig } from "next";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig: NextConfig = {
-	typedRoutes: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'images.unsplash.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
+  typedRoutes: true,
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
